9b19ed77f68705c9594a83608a6e72054df893f0
Tirdy <antoine.duplouy@edu.devinci.fr>
2020-02-07 18:00:39 +0100

    Adding work

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,6 +16,9 @@ class HomeController extends Controller
     public function index()
     {
         /* TODO Récupérer tout les utilisateurs, et renvoyer la view "home.blade.php avec les utilisateurs" */
+        $users = User::all();
+
+        return view('home', compact('users'));
     }
 }
 
